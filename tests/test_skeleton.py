d51099f5f7364f9626e9e0e73eaca9d8a8afe6b8
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-08-10 02:12:45 +0100

    fix: tests

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -1,16 +1,3 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from gaanadl.skeleton import fib
-
-__author__ = "Aditya Gupta"
-__copyright__ = "Aditya Gupta"
-__license__ = "mit"
-
-
-def test_fib():
-    assert fib(1) == 1
-    assert fib(2) == 1
-    assert fib(7) == 13
-    with pytest.raises(AssertionError):
-        fib(-10)
